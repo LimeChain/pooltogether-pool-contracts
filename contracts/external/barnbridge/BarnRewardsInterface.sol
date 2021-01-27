652f66f5228d1cc0b54bdbf829c319d95afbcec6
Stanislav Trenev <stanislav.trenev@limechain.tech>
2021-01-27 09:15:11 +0200

    Update balance() logic

--- contracts/external/barnbridge/BarnRewardsInterface.sol
+++ contracts/external/barnbridge/BarnRewardsInterface.sol
@@ -4,4 +4,6 @@ interface BarnRewardsInterface {
     function claim() external;
     function owed(address _address) external view returns (uint256);
     function registerUserAction(address _user) external;
+    function currentMultiplier() external view returns (uint256);
+    function userMultiplier(address _user) external view returns (uint256);
 }
